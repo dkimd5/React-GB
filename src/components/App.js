7073870ffcf22074248204291491f7466eb029d2
dkimd5 <dkimd5@mail.ru>
2021-04-28 11:18:35 +0300

    points 1-5 done

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState, useCallback } from "react";
+import Message from "./Message";
+
+export default function App() {
+   return <Message text="From the Future" />
+}
---
+++ src/components/Message.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Message(props) {
+   return <h1>Hello {props.text}</h1>
+}
