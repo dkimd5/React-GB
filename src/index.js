a7fc08b4452e17b13aabaa0d1c9893896e228ee2
dkimd5 <dkimd5@mail.ru>
2021-05-29 10:57:40 +0300

    service worker created and registered

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./store";
 import { PersistGate } from 'redux-persist/integration/react'
 
+// if ('serviceWorker' in navigator) {
+//    navigator.serviceWorker.register('/src/sw.js').catch((err) => console.log(err));
+//    console.log("SW register success");
+// }
+
+window.addEventListener('load', async () => {
+   if ('serviceWorker' in navigator) {
+     try {
+       const reg = await navigator.serviceWorker.register('/src/sw.js')
+       console.log('Service worker register success', reg)
+     } catch (e) {
+       console.log('Service worker register fail')
+     }
+   }
+ })
+
 ReactDOM.render(
    <Provider store={ store }>
       <PersistGate persistor={ persistor } loading={ null }>
