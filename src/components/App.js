7df3749ddbcd82f2324b1a6579df9d4b75b583c7
dkimd5 <dkimd5@mail.ru>
2021-10-17 09:35:16 +0300

    pathes changed, routes cleared, profile link deleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import { Header } from "./Header/Header";
-import { Messenger } from "./ChatsWindow/Messenger";
+import { Messenger } from "./Messenger/Messenger";
 import "./reset.css";
 import "./app.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Profile } from "./Profile/Profile";
-
 
 
 export default function App() {
@@ -20,9 +18,6 @@ export default function App() {
           </Route>
           <Route path="/chat/:chatId">
             <Messenger />
-          </Route>
-          <Route path="/profile">
-            <Profile />
           </Route>
         </Switch>
       </div>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 export const Header = () => {
    return (
       <div className="header">
-         <Link to="/profile" className="header-link link">Profile</Link>
          <Link to="/" className="header-text link">
             <h1>React Messenger</h1>
          </Link>
