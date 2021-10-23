d8d0bb5974f772a283465d1208933f228f708c64
dkimd5 <dkimd5@mail.ru>
2021-10-23 10:39:03 +0300

    .env added

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const Header = () => {
    return (
       <div className="header">
-         <Link to="/" className="header-text link">
+         <Link to="/" className="header-text">
             <h1>React Messenger</h1>
          </Link>
       </div>
