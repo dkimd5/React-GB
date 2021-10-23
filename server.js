981b064198c963468a591c3ffb8aad4731c1b4a2
dkimd5 <dkimd5@mail.ru>
2021-10-23 12:35:32 +0300

    meta tag csp added

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const path = require('path');
 
 const port = process.env.PORT || 3001;
 
-const csp = "default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://www.google.com"
+// const csp = "default-src *; style-src * 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' http://www.google.com"
+
+// , {
+//   setHeaders: function (res, path) {
+//     res.set('Content-Security-Policy', csp)
+//   },
+// }
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('build', {
-    setHeaders: function (res, path) {
-      res.set('Content-Security-Policy', csp)
-    },
-  }));
+  app.use(express.static('build'));
   app.get('*', (req, res) => {
     req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   })
